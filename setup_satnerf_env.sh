dc3db368b4d9b4ddb7de4ecdc7d4ff2efa17339e
Zaki Indra Sukma <indra.zaki@gmail.com>
2023-08-12 20:23:36 +0000

    update pytorch lightning version to v2.0

--- setup_satnerf_env.sh
+++ setup_satnerf_env.sh
@@ -1,7 +1,10 @@
 ### create satnerf venv
+
+set -e
+
 conda create -n satnerf -c conda-forge python=3.8 libgdal
 conda activate satnerf
 pip install --ignore-installed certifi -r requirements.txt
-pip install torch==1.7.1+cu110 torchvision==0.8.2+cu110 torchaudio===0.7.2 -f https://download.pytorch.org/whl/torch_stable.html
+#pip install torch==1.7.1+cu110 torchvision==0.8.2+cu110 torchaudio===0.7.2 -f https://download.pytorch.org/whl/torch_stable.html
 conda deactivate
 echo "satnerf conda env created !"
